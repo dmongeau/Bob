25fbab7da8769443507ccba8965f320645866ab4
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2011-04-27 07:58:00 -0400

    Added create method

--- Bob.php
+++ Bob.php
@@ -10,17 +10,53 @@ require PATH_BOB.'/functions/functions.php';
 
 class Bob {
 	
+	public static $config = array(
+		'namespace' => 'Bob'
+	);
 	
+	/*
+	 *
+	 * Method to load files and classes that are needed
+	 *
+	 *
+	 */
 	public static function need($name) {
 		
 		
 		
 	}
 	
-	
-	public static function create($name,$args) {
+	/*
+	 *
+	 * Method to create instance of objects
+	 *
+	 *
+	 */
+	public static function create($name) {
 		
+		$className = str_replace(' ','_',ucwords(str_replace('_',' ',$name)));
+		if(!empty(self::$config['namespace'])) $className = self::$config['namespace'].'_'.$className;
 		
+		if(class_exists($className)) {
+			
+			switch (func_num_args()) {
+				case 2:
+					return new $className(func_get_arg(1));
+				case 3:
+					return new $className(func_get_arg(1), func_get_arg(2));
+				case 4:
+					return new $className(func_get_arg(1), func_get_arg(2), func_get_arg(3));
+				case 5:
+					return new $className(func_get_arg(1), func_get_arg(2), func_get_arg(3), func_get_arg(4));
+				case 6:
+					return new $className(func_get_arg(1), func_get_arg(2), func_get_arg(3), func_get_arg(4), func_get_arg(5));
+				default:
+					return new $className();
+			}
+			
+		} else {
+			throw new Exception('Object does not exist');	
+		}
 		
 		
 	}
