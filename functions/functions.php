d3a43b5b797c1b6a071a3fa64407f2cd374b55b5
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2011-05-16 13:40:11 -0400

    Added uuid function

--- functions/functions.php
+++ functions/functions.php
@@ -1,3 +1,14 @@
 <?php
 
 
+
+function bob_uuid() {
+	
+	$md5 = md5(uniqid('', true));
+	return substr($md5, 0, 8 ) . '-' .
+			substr($md5, 8, 4) . '-' .
+			substr($md5, 12, 4) . '-' .
+			substr($md5, 16, 4) . '-' .
+			substr($md5, 20, 12);
+	
+}
